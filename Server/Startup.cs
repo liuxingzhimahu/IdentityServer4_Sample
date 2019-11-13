fec4e624366f77c371b564cf5d8d87a22f272d2b
jonny.he <421084478@qq.com>
2019-11-13 21:40:35 +0800

    commit init

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -16,6 +16,12 @@ namespace Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources()
+                .AddInMemoryClients()
+                .AddInMemoryIdentityResources()
+                .AddTestUsers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +34,11 @@ namespace Server
 
             app.UseRouting();
 
+            app.UseIdentityServer();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
